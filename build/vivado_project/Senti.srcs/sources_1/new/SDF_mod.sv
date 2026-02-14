6928f449ee2e01c669fc92849c6d181ab6f57b7e
Ratire <Ratire7@gmail.com>
2026-02-14 12:20:50 -0500

    Made some additions to the SDF module and now it should work for the most part.

--- build/vivado_project/Senti.srcs/sources_1/new/SDF_mod.sv
+++ build/vivado_project/Senti.srcs/sources_1/new/SDF_mod.sv
@@ -13,14 +13,12 @@ module SDF_mod #(parameter int S = 1, POINTS = 512) //S represents the stage thi
     localparam STRIDE = 2**(S-1);
     
     //Connecting signals between modules
-    logic bf_on, rst_reg;
+    logic bf_on;
     logic [DATA_W-1:0] twiddle;
     logic [DATA-1:0] data_out_int, delay_in_int, delay_out;
     
-    assign rst_reg = (!val_in || rst);
-    
     Butterfly_FSM #(.MTI(DELAY_BUFFER_SIZE), .STRIDE(STRIDE)) bf_fsm (.val_in(val_in), .clk(clk), .rst(rst), .twiddle(twiddle), .bf_on(bf_on), .val_out(val_out));
-    Shift_Reg #(.REG_SIZE(DELAY_BUFFER_SIZE)) shift_reg (.delay_in(delay_in_int), .clk(clk), .rst(rst_reg), .delay_out(delay_out));
+    Shift_Reg #(.REG_SIZE(DELAY_BUFFER_SIZE)) shift_reg (.delay_in(delay_in_int), .clk(clk), .rst(rst), .val_in(val_in), .delay_out(delay_out));
     Butterfly bf (.data_in(data_in), .delay_out(delay_out), .twiddle(twiddle), .bf_on(bf_on), .data_out(data_out), .delay_in(delay_in_int));
     
 endmodule
@@ -38,30 +36,30 @@ module tb_SDF_mod;
     
     task automatic wait_c(input int j); //lets j-1 cycles pass and stops simulation at negedge of clk
         for(int i = 0; i < j; ++i)
-            @(negedge clk);
+            @(posedge clk);
     endtask
     
     task automatic wait_incr(input int j); //lets j-1 cycles pass and stops simulation at negedge of clk
         for(int i = 0; i < j; ++i)
         begin
-            @(negedge clk);
+            @(posedge clk);
             data_in <= data_in + 1;
         end
     endtask
     
     initial
     begin
-        wait_c(2);
+        wait_c(1);
         
         rst <= 1;
         
-        wait_c(2);
-        
-        val_in <= 1;
-        
-        wait_c(2);
+        wait_c(1);
         
         rst <= 0;
+        
+        wait_c(1);
+        
+        val_in <= 1;
         
         wait_incr(30);
         
--- build/vivado_project/Senti.srcs/sources_1/new/Shift_Reg.sv
+++ build/vivado_project/Senti.srcs/sources_1/new/Shift_Reg.sv
@@ -4,22 +4,22 @@ import FFT_pkg::*; // Import all definitions
 module Shift_Reg #(parameter int REG_SIZE)
     (
     input logic [DATA-1:0] delay_in,
-    input logic clk, rst,
+    input logic clk, rst, val_in,
     output logic [DATA-1:0] delay_out
     );
     
     logic [REG_SIZE-1:0][DATA-1:0] reg_array = '{default: '0};
+    
+    assign delay_out = reg_array[REG_SIZE-1];
     
     always_ff @(posedge clk or posedge rst)
         begin
         priority if (rst)
             begin
             reg_array <= '{default: '0};
-            delay_out <= '0;
             end
-        else
+        else if (val_in)
             begin
-            delay_out <= reg_array[REG_SIZE-1];
             reg_array <= {reg_array[REG_SIZE-2:0], delay_in};
             end
         end
@@ -28,12 +28,12 @@ endmodule
 
 
 module tb_Shift_Reg;
-    logic clk = 0, rst = 1, val_internal = 0;
+    logic clk = 0, rst = 1, val_in = 0;
     logic [DATA-1:0] delay_in_var = '0, delay_out_var;
     
     localparam int REG_SIZE_TB = 5;
     
-    Shift_Reg #(.REG_SIZE(REG_SIZE_TB)) dut (.clk(clk), .val_internal(val_internal), .rst(rst), .delay_in(delay_in_var), .delay_out(delay_out_var));
+    Shift_Reg #(.REG_SIZE(REG_SIZE_TB)) dut (.clk(clk), .val_in(val_in), .rst(rst), .delay_in(delay_in_var), .delay_out(delay_out_var));
     
     always #5 clk = ~clk;
     always @(posedge clk)
